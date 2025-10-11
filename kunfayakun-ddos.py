5e0ebde1f227241be6850409070a42884593e845
Zan-0xy <ahmadsan272@gmail.com>
2025-10-11 14:07:26 +0700

    Update kunfayakun-ddos.py

--- kunfayakun-ddos.py
+++ kunfayakun-ddos.py
@@ -64,7 +64,7 @@ def print_red_centered_art():
     os.system("clear")
     art = '''
              ╔════════╗
-             ║             ║
+             ║         ║
              ║╔═══════╝
              ║║
              ║║
@@ -72,9 +72,9 @@ def print_red_centered_art():
       ╚═╝  ╔═╝║
 ╔╗         ║╔═╝
 ║╚═════════╝║
- ║         ║
- ╚╗      ╔╝ 
-   ╚═══════╝
+ ║          ║
+ ╚╗        ╔╝ 
+  ╚═══════╝
 █'''   
     red_art = f"{Fore.GREEN}{art}{Style.RESET_ALL}"  # Set th text color to red
     print(red_art.center(80))  # Adjust the width (80 characters) to match your terminal size
